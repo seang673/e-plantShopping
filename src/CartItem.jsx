988ca3c0e446ed5cce0bf0d4f83ac7912a776940
seang673 <seangriffiths673@gmail.com>
2025-08-21 18:58:58 -0400

    Cart Items Update

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    var total = 0;
+    cart.forEach((item) => {
+        let c = parseFloat(item.cost.substring(1));
+        total += c * item.quantity;
+    })
+    return total;
  
   };
 
   const handleContinueShopping = (e) => {
+    onContinueShopping(e);
    
   };
-
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity +1}));
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 0){
+        dispatch(updateQuantity({name:item.name, quantity:item.quantity -1}));
+    } else {
+        dispatch(removeItem(item));
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let c = parseFloat(item.cost.substring(1));
+    return c * item.quantity;
   };
 
   return (
